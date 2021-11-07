e7277c77de47ef4855dfcb91b9cd186a32c26645
Takamatsu.kyoko <38364117+takamatsu-kyoko@users.noreply.github.com>
2021-11-08 08:20:22 +0900

    Add blog index page

---
+++ src/pages/blog/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import BaseLayout from '../../layouts/base_layout'
+import BlogGrid from '../../components/blog_grid'
+
+export default function BlogIndex({ data }) {
+    return (
+        <BaseLayout>
+            <h1>Blog Index</h1>
+            <BlogGrid blog_posts={ data.allMdx.edges }/>
+        </BaseLayout>
+    )
+}
+
+export const query = graphql`
+query {
+  allMdx(
+    filter: {
+      fields: {
+        fileSourceInstanceName: {eq: "blog"},
+        fileName: {eq: "index"}
+      },
+      frontmatter: {status: {nin: ["draft"]}}
+    }
+    sort: {fields: frontmatter___published_on, order: ASC}
+  ) {
+    edges {
+      node {
+        id
+        slug
+        frontmatter {
+          title
+          categories
+          published_on(formatString: "YYYY-MM-DD")
+          updated_on(formatString: "YYYY-MM-DD")
+        }
+      }
+    }
+  }
+}
+`
