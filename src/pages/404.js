9e6de20e53e59cf5c2d0dea71e803ba1f42c409d
Takamatsu.kyoko <38364117+takamatsu-kyoko@users.noreply.github.com>
2021-11-07 21:43:34 +0900

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import BaseLayout from '../layouts/base_layout'
+
+export default function PageNotFound() {
+    return (
+        <BaseLayout>
+            <h1>404 Not found</h1>
+            <p>お探しのページは見つかりませんでした。</p>
+        </BaseLayout>
+    )
+}
